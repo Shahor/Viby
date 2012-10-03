2ec5d1720659b630b091fb596e884fc166dac3c9
Florent Solt <florent@solt.biz>
2012-10-03 18:43:39 +0200

    Refactoring and improvements
    Bundler suport
    Yaml config file
    Remove Pathname usage
    Const name in upper case
    Remove some useless private methods
    Rewirte the Soptify plugin
    Remove twitter gem and config file
    Dailymotion & Vimeo videos support

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "http://rubygems.org"
+gem "cinch"
+gem "curb"
+gem "nokogiri"
--- plugins/Spotify.cinch.plugin.rb
+++ plugins/Spotify.cinch.plugin.rb
@@ -1,53 +1,30 @@
-#!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
-# Copyright (c) 2010 Victor Bergöö
-# This program is made available under the terms of the MIT License.
-# https://github.com/netfeed/cinch-spotify
+# Inspired by https://github.com/netfeed/cinch-spotify
 
-require 'cinch'
 require 'json'
-require 'curb'
+require 'open-uri'
 
 class Spotify
-    include Cinch::Plugin
+  include Cinch::Plugin
 
-    match /(spotify:(album|track|artist):[a-zA-Z0-9]+)/, :use_prefix => false
-    match /(http:\/\/open.spotify.com\/(album|track|artist)\/[a-zA-Z0-9]+)/, :use_prefix => false
+  match /(spotify:(album|track|artist):[a-zA-Z0-9]+)/, :use_prefix => false
+  match /(http:\/\/open.spotify.com\/(album|track|artist)\/[a-zA-Z0-9]+)/, :use_prefix => false
 
-    def execute m, uri, type
-        msg = case type
-        when /artist/ then _artist uri
-        when /album/ then _album uri
-        when /track/ then _track uri
-        else 'something went wrong'
-        end
+  def execute m, uri, type
+    data = JSON.parse(open("http://ws.spotify.com/lookup/1/.json?uri=#{uri}").read)
 
-        m.reply("Spotify: #{msg}")
-    end
+    msg = case type
+          when /artist/
+            "Artist: #{data['artist']['name']}"
+          when /album/
+            "Album: #{data['album']['name']} by #{data['album']['artist']}"
+          when /track/
+            "Track: #{data['track']['name']} by #{data['track']['artists'].first['name']} (#{data['track']['album']['name']})"
+          else
+            "Something went wrong"
+          end
 
-    def _artist uri
-        json = _lookup uri
-        name = json['artist']['name']
-        "Artist: #{name}"
-    end
+    m.reply "Spotify: #{msg}"
+  end
 
-    def _album uri
-        json = _lookup uri
-        artist = json['album']['artist']
-        album = json['album']['name']
-        "Album: #{album} by #{artist}"
-    end
-
-    def _track uri
-        json = _lookup uri
-        artist = json['track']['artists'].first['name']
-        album = json['track']['album']['name']
-        track = json['track']['name']
-        "Track: #{track} by #{artist} (#{album})"
-    end
-
-    def _lookup spotify_uri
-        content = Curl::Easy.perform("http://ws.spotify.com/lookup/1/.json?uri=#{spotify_uri}").body_str
-        JSON.parse(content)
-    end
 end
