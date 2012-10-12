1cf838d771e34c57fd963f0bc17cd11d778874ca
Florent Solt <florent@solt.biz>
2012-10-12 16:59:38 +0200

    Add team command

---
+++ plugins/Team.cinch.plugin.rb
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+class Joke
+  include Cinch::Plugin
+
+  match /(?:team|quake|q3|ctf) ((?:[\w\-]+\s*)+)/i
+
+  set :help => "!team or !quake or !q3 or !ctf followed by the space separated player list"
+
+  def execute m, players
+    players = players.strip.gsub(/\s+/, ' ').split(' ')
+    blue = players.sample(players.size / 2)
+    red = players - blue
+    m.reply "BLUE: #{blue.join(" ")}"
+    m.reply "RED: #{red.join(" ")}"
+  end
+
+end
