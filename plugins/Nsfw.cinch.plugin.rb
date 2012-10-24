0bd8ef7be898eb8d1922d334fb0cee127a9c95c7
Shahor <shahor@shahor.fr>
2012-10-24 10:47:00 +0200

    Restrictions, restrictions, restrictions

--- plugins/Nsfw.cinch.plugin.rb
+++ plugins/Nsfw.cinch.plugin.rb
@@ -2,36 +2,92 @@
 require 'nokogiri'
 require 'open-uri'
 
-
-
 class Nsfw
+    attr_accessor :alreadySeen, :lastFlush
     include Cinch::Plugin
 
     set :help => "just type in '!nsfw' and I will provide"
 
-    SOURCES = [
+    HIGHLY_NSFW = [
         "underboob",
-        "boobs",
         "burstingout",
         "downblouse",
         "curvy",
-        "hugeboobs",
-        "randomsexiness",
         "tittydrop",
-        "NSFWFunny",
+        "nsfw"
     ]
+
+    MIDDLY_NSFW = [
+        "randomsexiness",
+        "boobies",
+        "boobs",
+        "gonewildcurvy",
+        "LegalTeens",
+        "Hotchickswithtattoos",
+        "SexyButNotPorn",
+        "nude",
+        "Unashamed",
+        "GirlsinTUBEsocks"
+    ]
+
     match /nsfw$/i, :use_prefix => true
+    def initialize(*args)
+        super
+        if not config.include? "reddit_cookie"
+            raise "This plugin won't run unless you specify a cookie value for reddit [look for the over18 cookie in a nsfw board]"
+        end
+
+        @lastFlush = Time.now.to_i
+        @alreadySeen = []
+        @lastSeenPerUser = {}
+    end
 
     def execute m
-        open_doc = open("http://reddit.com/r/#{SOURCES.sample}" , "Cookie" => "over18=2ca6895c65d7a8e0ce05be36e92ec184e549e5fe")
-        doc = Nokogiri::HTML(open_doc)
+        if not userCan? m.user.nick
+            m.reply "Oh #{m.user.nick}, you already had your treat today !"
+            return nil
+        end
 
-        links = doc.css('a.title').to_a
+        board = if Time.now.hour >= 18
+            HIGHLY_NSFW.sample
+        else
+            MIDDLY_NSFW.sample
+        end
+
+        open_doc = open("http://reddit.com/r/#{board}" , "Cookie" => "over18=#{config['reddit_cookie']}")
+        links = Nokogiri::HTML(open_doc).css('a.title').to_a
+
+        begin
+            link = getImageFromLinks links
+        end while @alreadySeen.include? link
+
+        flushCashIfNeeded
+        @lastSeenPerUser[m.user.nick] = Time.now
+        @alreadySeen.push link
+        m.reply link
+    end
+
+    private
+
+    def userCan? user
+        return !(@lastSeenPerUser.has_key?(user) && @lastSeenPerUser[user].day == Time.now.day)
+    end
+
+    def flushCashIfNeeded
+        now = Time.now.to_i
+
+        if (now - @lastFlush) > 604800 # more than 7 days old
+            @lastFlush = now
+            @alreadySeen.clear
+        end
+    end
+
+    def getImageFromLinks links
         link = links.sample.attribute('href').to_s
         while link !~ /(jpg|gif|png)$/i
             link = links.sample.attribute('href').to_s
         end
 
-        m.reply link
+        return link
     end
 end
