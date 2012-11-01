44eed0a0cf69fe4700cd446c76c6a79df3542397
Quentin Cuvillier <Quentin.cuvillier@utbm.fr>
2012-11-01 01:42:23 +0100

    Idendation issue

--- plugins/Bieber.cinch.plugin.rb
+++ plugins/Bieber.cinch.plugin.rb
@@ -13,14 +13,14 @@ class Bieber
   match /(^|\s)(Bieber)/i, :use_prefix => false
 
   def execute m
-	  try = 0
-	 	begin
-	 		try = try + 1
-	 		open_doc = open("https://gdata.youtube.com/feeds/api/videos/#{VIDEOS.sample}/comments?max-results=50")
-	 		comments = Nokogiri::HTML(open_doc).search('content').to_a
-	 		candidates = comments.select {|e| (REGEX_INC.match(e) && !REGEX_EXC.match(e)) }
-	 		comment = candidates.sample
-	 	end while (try < VIDEOS.count && comment == nil)
-	 	m.reply comment != nil ? comment.text : "I found no funny comments to share :-( "
-	end
+    try = 0
+    begin
+      try = try + 1
+      open_doc = open("https://gdata.youtube.com/feeds/api/videos/#{VIDEOS.sample}/comments?max-results=50")
+      comments = Nokogiri::HTML(open_doc).search('content').to_a
+      candidates = comments.select {|e| (REGEX_INC.match(e) && !REGEX_EXC.match(e)) }
+      comment = candidates.sample
+    end while (try < VIDEOS.count && comment == nil)
+    m.reply comment != nil ? comment.text : "I found no funny comments to share :-( "
+  end
 end
