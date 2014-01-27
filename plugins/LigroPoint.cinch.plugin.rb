33d310fab3175ad5bcfc1f6d1e92d7076f3dc937
Alexandre Gaudencio <shahor@shahor.fr>
2014-01-27 17:26:07 +0100

    Ligropoints
    Clean code
    Add storage

--- plugins/LigroPoint.cinch.plugin.rb
+++ plugins/LigroPoint.cinch.plugin.rb
@@ -1,41 +1,76 @@
 # -*- coding: utf-8 -*-
 
 class LigroPoint
-  include Cinch::Plugin
+    include Cinch::Plugin
 
-  match(/ligropoints?(?:\s+(\S{1,20})(?:\s+([+-]?\d{1,10}))?)?/i)
+    @@fileName = "ligropoints.yml"
 
-  set :help => "!ligropoint [nick] [+-count] :\n * if nick is 'top' or unspecified, print the top score.\n * if count is 0 or uspecified, print the user score.\n * else give some ligropoints to the user."
+    attr_accessor :points
 
-  def initialize(*args)
-    super
-    @points = {}
-    @points.default = 0
-  end
+    match(/ligropoints?(?:\s+(\S{1,20})(?:\s+([+-]?\d{1,10}))?)?/i)
 
-  def execute m, nick, count
-    count = count.to_i
+    set :help => "!ligropoint [nick] [+-count] :\n * if nick is 'top' or unspecified, print the top score.\n * if count is 0 or uspecified, print the user score.\n * else give some ligropoints to the user."
 
-    if nick == 'top' or nick == nil
-      top = @points.sort_by {|key, value| -value}
-      for nick, count in top[0, 5]
-        m.reply "#{nick.ljust(10)}: #{count}"
-      end
-    elsif nick == m.user.nick and count != 0
-      m.reply 'You can\'t change your ligropoint count!'
-    else
-      @points[nick] += count
-      if @points[nick] == 0
-        @points.delete(nick)
-      end
-      if count == 0
-        m.reply "#{nick} has #{@points[nick]} ligropoints"
-      elsif count < 0
-        m.reply "#{m.user.nick} removes #{-count} ligropoints to #{nick}"
-      else
-        m.reply "#{m.user.nick} gives #{count} ligropoints to #{nick}"
-      end
+    def initialize(*args)
+        super
+        begin
+            @points = YAML::load_file(Dir.pwd + LigroPoint.fileName) #Load
+        rescue
+            @points = {}
+        end
+        @points.default = 0
     end
-  end
 
+    def execute m, nick, count
+        count = count.to_i
+
+        if nick == m.user.nick and count != 0
+            m.reply 'You can\'t change your ligropoint count!'
+            return
+        end
+
+        if nick == 'top' or nick == nil
+            getScores m
+        else
+            addPoints m, nick, count
+        end
+    end
+
+    def getScores m
+        top = @points.sort_by { |key, value|
+            -value
+        }
+
+        for nick, count in top[0, 5]
+            m.reply "#{nick.ljust(10)}: #{count}"
+        end
+    end
+
+    def addPoints m, nick, count
+        @points[nick] += count
+
+        if @points[nick] == 0
+            @points.delete(nick)
+        end
+
+        if count == 0
+            m.reply "#{nick} has #{@points[nick]} ligropoints"
+        elsif count < 0
+            m.reply "#{m.user.nick} removes #{-count} ligropoints to #{nick}"
+        else
+            m.reply "#{m.user.nick} gives #{count} ligropoints to #{nick}"
+        end
+
+        updateYaml
+    end
+
+    def updateYaml
+        File.open(File.join(Dir.pwd, 'plugins', LigroPoint.fileName), 'w+') do |f|
+            f.write(@points.to_yaml)
+        end
+    end
+
+    def self.fileName
+        @@fileName
+    end
 end
