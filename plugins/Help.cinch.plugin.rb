db5658a6025c8e0e4fb3bf15a49a7e1c51df1489
Fiaxhs <lowreg@gmail.com>
2014-02-26 10:42:48 +0100

    List only helpful plugins

--- plugins/Help.cinch.plugin.rb
+++ plugins/Help.cinch.plugin.rb
@@ -6,7 +6,11 @@ class Help
   match /help$/i, :use_prefix => true
 
   def execute m
-    m.reply "Hey! You can ask for help on any of the following Plugins by typing '!help plugin_name' : #{m.bot.plugins.map{ |e| e.class.name.downcase  }.join(', ')}"
+    helpful = []
+    m.bot.plugins.each { |e| 
+      helpful << e if e.class.help != nil
+    }
+    m.reply "Hey! You can ask for help on any of the following Plugins by typing '!help plugin_name' : #{helpful.map{ |e| e.class.name.downcase  }.join(', ')}"
   end
 
 end
