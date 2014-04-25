c17bcc6d2425b81ead3875ea39d25905f7af34b5
Fiaxhs <lowreg@gmail.com>
2014-04-25 17:11:30 +0200

    Old now stores urls in a sqlite db

--- plugins/Older.cinch.plugin.rb
+++ plugins/Older.cinch.plugin.rb
@@ -1,54 +1,49 @@
 require 'open-uri'
+require 'sqlite3'
 
 class Older
   include Cinch::Plugin
-
-  MAX = 1000
-
-  PICTURES = [
-    "http://www.monaco-munich.de/images/galerie/74_dodge_monaco_blues_brothers_setpic.jpg",
-    "http://f.cl.ly/items/1X2L2f0L1j0Q1M2v1N0H/b6A9M.gif",
-    "http://play-auto.net/wp-content/uploads/2009/12/old-police-car.GIF",
-    "http://4.bp.blogspot.com/_8gyAih3p8fY/TVGzMTLbf8I/AAAAAAAABho/Jmr8x1ZD5AI/s1600/uncle-sam.jpg",
-    "http://foodfamilyfinds.com/wp-content/uploads/2010/05/illinois-police-memorial-old-police-car.jpg",
-    "http://icanhascheezburger.files.wordpress.com/2009/02/funny-pictures-your-cat-does-not-care.jpg",
-    "http://spoonfulpb.files.wordpress.com/2011/09/interested_lol_cat.jpg",
-  ]
-
-  SENTENCES = [
-    "This link is so OLD ... (posted by %{author} at %{date})",
-    "You've been olded by %{author} [posted at %{date}]",
-    "Bro, you're so OLD ! %{author} posted it at %{date}",
-    "Boooooyaaaaaaaaaaa ! %{author} just made you a swag older (posted on %{date})"
-  ]
 
   listen_to :channel, method: :considerOlding
 
   def initialize(*args)
     super
-    @links = Hash.new { |hsh, key| hsh[key] = {} }
+    @db = SQLite3::Database.new( "old.db" )
+    @db.execute <<-SQL
+      CREATE TABLE IF NOT EXISTS urls (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        link TEXT, 
+        who TEXT, 
+        tag TEXT, 
+        date DATETIME
+      );
+    SQL
+    @db.results_as_hash = true
   end
 
   def considerOlding(m)
-    urls = m.message.split.grep URI.regexp
-
-    if urls.any?
-      urls.each do |url|
-        next if not url =~ /^http/
-
-        channel_links = @links[m.channel.name]
-
-        if channel_links.key? url
-          if channel_links[url][:author] != m.user.nick
-            m.reply PICTURES.sample if rand <= 0.33
-            m.reply SENTENCES.sample % channel_links[url]
+    if m.message =~ /^!url ([^\s]*)(\s(\d+))?/
+      old = @db.execute "SELECT * FROM urls WHERE link LIKE ?", "%#{$1}%"
+      if !old.empty?
+        # Take last url by default
+        idx = $3 ? $3.to_i - 1 : old.length - 1
+        # stay in bounds
+        idx = [idx, old.length - 1].min
+        row = old[idx]
+        m.reply "#{row['link']} by #{row['who']}, #{row['date']} - (url #{idx+1}/#{old.length})"
+      else
+        m.reply "No results"
+      end
+    else
+      urls = m.message.split.grep URI.regexp
+      if urls.any?
+        urls.each do |url|
+          is_old = @db.execute "SELECT * FROM urls WHERE link = ?", url
+          if !is_old.empty?
+            m.reply "Old"
+          else 
+            @db.execute "INSERT INTO urls (link, who, tag, date) VALUES (?, ?, ?, ?)", url, m.user.nick, "", DateTime.now.strftime('%d/%m/%Y %H:%M:%S')
           end
-        else
-          channel_links[url] = {
-            author: m.user.nick,
-            date: Time.now.strftime("%Y-%m-%d %H:%M:%S"),
-          }
-          channel_links.shift if channel_links.size > MAX
         end
       end
     end
