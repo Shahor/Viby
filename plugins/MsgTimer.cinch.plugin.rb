4231e316e181092e41c4cee17c581edf32f95e8f
vincent <vincent.daligault@3ds.com>
2014-07-22 13:38:47 +0200

    add msg timer plugin

---
+++ plugins/MsgTimer.cinch.plugin.rb
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+require 'cinch'
+require 'date'
+
+class MsgTimer
+    include Cinch::Plugin
+
+    def initialize(*args)
+      super
+      @messages = []
+      for message in config
+          @messages.push(message)
+      end
+    end
+
+
+    timer 60, method: :timed
+    def timed
+        time = DateTime.now.strftime("%H:%M")
+        for message in @messages
+            Channel("#viby").send "tests #{message['time']} - #{time}"
+
+            if message['time'] == time
+                Channel(message['chan']).send message['msg']
+            end
+        end
+    end
+end
+
